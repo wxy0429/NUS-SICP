e0edde553e31292b31e7cf4931c3a8baff3fa98c
Source Academy User <No public email provided>
2025-07-14 10:45:46 +0800

    Changes made from Source Academy

---
+++ SICP.js
@@ -0,0 +1,21 @@
+function d_filter(pred, xs) {
+// ???  
+      if (is_null(xs)){
+         return null;
+      } else {
+          const wish = d_filter(pred,tail(xs));
+          set_tail(xs, wish);
+          return pred(head(xs))
+                 ? xs
+                 : tail(xs);
+      }
+             
+             
+          
+          
+      }
+
+
+const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
+d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
+L; // What is L now?
